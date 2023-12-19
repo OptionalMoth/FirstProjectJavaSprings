fe284916bd552a4e574356de934ad22f5730bb3b
Solo <ksolo15mtetwa@outlook.com>
2023-12-19 18:16:11 +0100

    Initial commit

---
+++ src/main/java/com/example/FirstProjectJavaSprings/FirstProjectJavaSpringsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstProjectJavaSprings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpringsApplication.class, args);
+	}
+
+}
