da3cdc71675c243f312e05012f1dbdbe41e3d630
Solo <ksolo15mtetwa@outlook.com>
2023-12-19 18:58:29 +0100

    New Html file and an image to Display on the Html File

--- src/main/java/com/example/FirstProjectJavaSprings/controller/HelloController.java
+++ src/main/java/com/example/FirstProjectJavaSprings/controller/HelloController.java
@@ -1,12 +1,22 @@
 package com.example.FirstProjectJavaSprings.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @RestController
 public class HelloController {
     @GetMapping(value = "/")
     public String hello(){
         return "Hi ,This is my first Spring Controller";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false,defaultValue = "World") String name,Model model){
+        model.addAttribute("name",name);
+        return "gretting";
     }
 }
