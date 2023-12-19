5146e7da5553bbcce4b4c84c45768436eb17c539
Solo <ksolo15mtetwa@outlook.com>
2023-12-19 19:33:35 +0100

    Fixed some errors and typos in the html file greeting.html and in the HelloController Class

--- src/main/java/com/example/FirstProjectJavaSprings/controller/HelloController.java
+++ src/main/java/com/example/FirstProjectJavaSprings/controller/HelloController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController
+//@RestController
 public class HelloController {
     @GetMapping(value = "/")
     public String hello(){
@@ -17,6 +17,6 @@ public class HelloController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false,defaultValue = "World") String name,Model model){
         model.addAttribute("name",name);
-        return "gretting";
+        return "greeting";
     }
 }
